ce178dc6912f3dd1129e9e1f920dfd72de7c0d75
josephjaniga <josephjaniga@gmail.com>
2015-03-18 21:37:41 -0400

    Updating animations and Lion Boss Controller

---
+++ Assets/Scripts/LionBossAnimator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LionBossAnimator : MonoBehaviour {
+	
+	public Animator a;
+	public CharacterMotion cm;
+	
+	// Use this for initialization
+	void Start () {
+		a = GetComponent<Animator>();
+		cm = GetComponent<CharacterMotion>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// walking
+		if ( ( cm.isMovingLeft || cm.isMovingRight ) && GetComponent<Rigidbody2D>().velocity.y == 0  ){
+			a.SetBool("isWalking", true);
+		} else {
+			a.SetBool("isWalking", false);
+		}
+
+		// attacking
+		
+	}
+}
